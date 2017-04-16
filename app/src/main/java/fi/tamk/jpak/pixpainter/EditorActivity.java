4ec5e0ab019591b57bc661fba811da4428587afa
Juuso Pakarinen <juusojpak@gmail.com>
2017-04-17 01:02:06 +0300

    Add XML layout resource for EditorActivity.

--- app/src/main/java/fi/tamk/jpak/pixpainter/EditorActivity.java
+++ app/src/main/java/fi/tamk/jpak/pixpainter/EditorActivity.java
@@ -1,14 +1,24 @@
 package fi.tamk.jpak.pixpainter;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import fi.tamk.jpak.pixpainter.colorpicker.ColorPickerDialog;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private PixelGridView pixelgrid;
+    private Color primaryColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
         Intent intent = getIntent();
         int cols = 1;
         int rows = 1;
@@ -18,9 +28,25 @@ public class EditorActivity extends AppCompatActivity {
             rows = intent.getExtras().getInt("rows");
         }
 
-        PixelGridView pixelGrid = new PixelGridView(this, null);
-        pixelGrid.setNumColumns(cols);
-        pixelGrid.setNumRows(rows);
-        setContentView(pixelGrid);
+        pixelgrid = new PixelGridView(this, null);
+        pixelgrid.setNumColumns(cols);
+        pixelgrid.setNumRows(rows);
+
+        LinearLayout editorLayout = (LinearLayout) findViewById(R.id.rootLayout)
+                .findViewById(R.id.editorLayout);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+
+        if (editorLayout != null) {
+            editorLayout.addView(pixelgrid, params);
+        }
+    }
+
+    public void colorPickerClick(View v) {
+        ColorPickerDialog cpDialog = new ColorPickerDialog();
+        cpDialog.show(getFragmentManager(), "ColorPickerDialog");
     }
 }
