0c73d629945551c3ef9395da35558ea7cde76c28
Juuso Pakarinen <juusojpak@gmail.com>
2017-04-17 04:39:21 +0300

    Add listener for color picker.

---
+++ app/src/main/java/fi/tamk/jpak/pixpainter/colorpicker/ColorPickerListener.java
@@ -0,0 +1,11 @@
+package fi.tamk.jpak.pixpainter.colorpicker;
+
+import fi.tamk.jpak.pixpainter.ColorARGB;
+
+/**
+ * Created by Juuso Pakarinen on 17/04/2017.
+ */
+public interface ColorPickerListener {
+
+    void onColorChanged(ColorARGB color);
+}
