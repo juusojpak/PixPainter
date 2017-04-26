6cb3c53b815b24bc772ead9a70aa2b8a82152c6a
Juuso Pakarinen <juusojpak@gmail.com>
2017-04-26 17:43:09 +0300

    Add functionality to save drawing to Android Gallery.

--- app/src/main/java/fi/tamk/jpak/pixpainter/EditorActivity.java
+++ app/src/main/java/fi/tamk/jpak/pixpainter/EditorActivity.java
@@ -1,14 +1,26 @@
 package fi.tamk.jpak.pixpainter;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import fi.tamk.jpak.pixpainter.colorpicker.ColorPickerDialog;
 import fi.tamk.jpak.pixpainter.colorpicker.ColorPickerListener;
@@ -17,7 +29,6 @@ import fi.tamk.jpak.pixpainter.tools.PaintBucket;
 import fi.tamk.jpak.pixpainter.tools.Pencil;
 import fi.tamk.jpak.pixpainter.tools.Shape;
 import fi.tamk.jpak.pixpainter.tools.Tool;
-import fi.tamk.jpak.pixpainter.tools.ToolType;
 
 public class EditorActivity extends AppCompatActivity implements ColorPickerListener {
 
@@ -116,6 +127,10 @@ public class EditorActivity extends AppCompatActivity implements ColorPickerList
         highlightToolButton();
     }
 
+    public void handleSaveClick(View v) {
+        saveImageToFile();
+    }
+
     @Override
     public void onColorChanged(ColorARGB color) {
         this.primaryColor = color;
@@ -148,6 +163,62 @@ public class EditorActivity extends AppCompatActivity implements ColorPickerList
             case FILL:
                 toolButtons.get(3).setTextColor(activeColor);
                 break;
+        }
+    }
+
+    public void saveImageToFile() {
+        FileOutputStream outStream = null;
+
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+            String dateStr = dateFormat.format(new Date());
+            System.out.println(dateStr);
+            String fileName = dateStr +".jpg";
+            outStream =  this.openFileOutput("testing.png", Context.MODE_PRIVATE);
+
+            //Define a bitmap with the same size as the view
+            Bitmap bitmap = Bitmap.createBitmap(pixelgrid.getWidth(),
+                    pixelgrid.getHeight(), Bitmap.Config.ARGB_8888);
+
+
+            //Bind a canvas to it
+            Canvas canvas = new Canvas(bitmap);
+
+            //Get the view's background
+            Drawable bgDrawable = pixelgrid.getBackground();
+
+            if (bgDrawable != null) {
+                //has background drawable, then draw it on the canvas
+                bgDrawable.draw(canvas);
+            } else {
+                //does not have background drawable, then draw white background on the canvas
+                canvas.drawColor(Color.WHITE);
+            }
+
+            // draw the view on the canvas
+            pixelgrid.draw(canvas);
+
+            // Bitmap to file
+            // bitmap.compress(Bitmap.CompressFormat.PNG, 80, outStream);
+
+            // Bitmap to Gallery (as .jpg)
+            MediaStore.Images.Media.insertImage(getContentResolver(), bitmap,
+                    fileName, ("PixPainter image, created " + dateStr));
+
+            Toast.makeText(this,
+                    getResources().getString(R.string.savedToGalleryToast),
+                    Toast.LENGTH_SHORT).show();
+
+        } catch (FileNotFoundException | SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (outStream != null) {
+                    outStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
